be6718349ca2301c7b653f13c4cb8b1552f2f063
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2022-08-31 03:40:47 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function fun() {  
+var a = document.getElementById("para1");  
+a.classList.remove ("active");  
+}  
+
+function fun2() {  
+    var b = document.getElementById("para2");  
+    b.classList.remove ("active");  
+    }
+ 
